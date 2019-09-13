a70611159c4c4336ad3ebee0a105ff4a6f9d2372
Victor Gonzalez <dazedbutamused@gmail.com>
2019-09-13 16:33:10 -0700

    server folder change

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myCarDiaryApp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_diary.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- sdafd/wsgi.py
+++ sdafd/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for car_diary project.
+WSGI config for myCarDiaryApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_diary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myCarDiaryApp.settings')
 
 application = get_wsgi_application()
