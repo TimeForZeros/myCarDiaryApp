7667e7584098f6085331f77b648ee736b0f77e70
Timothy Cross <timothycross1994@gmail.com>
2019-09-17 11:43:19 -0700

    user should be able to add a photo

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import MaintenanceForm
 
-class MaintenanceForm(ModelForm):
-  class Meta:
-    model = Maintenance
-    fields = ['date', 'maintenace']
+# class MaintenanceForm(ModelForm):
+#   class Meta:
+#     model = Maintenance
+#     fields = ['date', 'maintenace']
