6fdb411f73756a0b5f881e063751becbb0a310b9
Victor Gonzalez <dazedbutamused@gmail.com>
2019-09-18 11:31:26 -0700

    Config Deployment Update

--- car_diary/settings.py
+++ car_diary/settings.py
@@ -122,7 +122,5 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/cars/'
 LOGOUT_REDIRECT_URL = '/'
 
-
-
-import django-heroku
-django-heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
